c7f2d456b7ed6a6ede684636e3e6d45d47c64cc9
Maciej Caderek <maciej.caderek@gmail.com>
2022-02-07 22:01:41 +0100

    WIP

--- src/main.tsx
+++ src/main.tsx
@@ -15,9 +15,6 @@ import { state, setState } from "./state";
 import { render } from "solid-js/web";
 import App from "./ui/App";
 
-const $moves = document.querySelector<HTMLImageElement>("#moves");
-const $controls = document.querySelector<HTMLImageElement>("#controls");
-
 const boardConfig: BoardConfig = {
   size: 1024,
   boardStyle: styles.calm,
