4374db019716e07e5434d18473ceac2b3d248187
Maciej Caderek <maciej.caderek@gmail.com>
2022-02-11 00:45:55 +0100

    WIP

--- src/test-data/pgns.ts
+++ src/test-data/pgns.ts
@@ -21,27 +21,6 @@ const pgns = [
 Qc4 31. Qxf6 Kxa3 32. Qxa6+ Kxb4 33. c3+ Kxc3 34. Qa1+ Kd2
 35. Qb2+ Kd1 36. Bf1 Rd2 37. Rd7 Rxd7 38. Bxc4 bxc4 39. Qxh8
 Rd3 40. Qa8 c3 41. Qa4+ Ke1 42. f4 f5 43. Kc1 Rd2 44. Qa7 1-0`,
-
-  `[Event "Paris"]
-[Site "Paris FRA"]
-[Date "1858.??.??"]
-[EventDate "?"]
-[Round "?"]
-[Result "1-0"]
-[White "Paul Morphy"]
-[Black "Duke Karl / Count Isouard"]
-[ECO "C41"]
-[WhiteElo "?"]
-[BlackElo "?"]
-[PlyCount "33"]
-
-1.e4 e5 2.Nf3 d6 3.d4 Bg4 {This is a weak move
-already.--Fischer} 4.dxe5 Bxf3 5.Qxf3 dxe5 6.Bc4 Nf6 7.Qb3 Qe7
-8.Nc3 c6 9.Bg5 {Black is in what's like a zugzwang position
-here. He can't develop the [Queen's] knight because the pawn
-is hanging, the bishop is blocked because of the
-Queen.--Fischer} b5 10.Nxb5 cxb5 11.Bxb5+ Nbd7 12.O-O-O Rd8
-13.Rxd7 Rxd7 14.Rd1 Qe6 15.Bxd7+ Nxd7 16.Qb8+ Nxb8 17.Rd8# 1-0`,
 ];
 
 export default pgns;
