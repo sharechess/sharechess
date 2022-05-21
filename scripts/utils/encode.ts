870a8b3811027d9501f6e8c43390a2d68c340d2d
Maciej Caderek <maciej.caderek@gmail.com>
2022-05-21 15:32:03 +0200

    Added error handling for piece stylesheets generation script

--- scripts/utils/encode.ts
+++ scripts/utils/encode.ts
@@ -1,10 +1,23 @@
 import fs from "fs";
+import path from "path";
 import mime from "mime";
 
+const ACCEPTED_FORMATS = new Set([".svg", ".pgn", ".jpg"]);
+
 const encode = (filePath: string) => {
-  const data = fs.readFileSync(filePath).toString("base64");
+  const stat = fs.lstatSync(filePath);
+  if (stat.isDirectory()) {
+    throw new Error(`Unexpected directory: ${filePath}`);
+  }
+
+  const ext = path.parse(filePath).ext;
+
+  if (!ACCEPTED_FORMATS.has(ext)) {
+    throw new Error(`Unexpected file format: ${filePath}`);
+  }
 
   const type = mime.getType(filePath);
+  const data = fs.readFileSync(filePath).toString("base64");
 
   return `data:${type};base64,${data}`;
 };
