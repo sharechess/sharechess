1f934cfa1b8c01a8164acf13fd525408d67d5495
Maciej Caderek <maciej.caderek@gmail.com>
2022-08-19 22:11:37 +0200

    Improved openingtree.com styles

--- scripts/style-templates/OpeningTreeBoardCSS.ts
+++ scripts/style-templates/OpeningTreeBoardCSS.ts
@@ -3,7 +3,11 @@ import { Style } from "../../src/types";
 const OpeningTreeBoardCSS = (dataURL: string, style: Style) => {
   return `
     @-moz-document domain("openingtree.com") {
-      .cg-wrap {background-image: ${dataURL} !important}
+      .cg-wrap {
+        background-image: ${dataURL} !important;
+        background-size: cover !important;
+        filter: none !important;
+      }
       square.last-move {background-color: ${style.moveIndicator.color} !important}
     }
   `;
