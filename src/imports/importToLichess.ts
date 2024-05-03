4f2f54c1340df59556f0f1d67e7b788c3322229c
Maciej Caderek <maciej.caderek@gmail.com>
2024-05-03 19:20:24 +0200

    Fixed lichess import

--- src/imports/importToLichess.ts
+++ src/imports/importToLichess.ts
@@ -1,28 +1,31 @@
 const importToLichess = async (pgn: string, site: string | null) => {
-  if (site && /^https:\/\/lichess\.org\/.{8}/.test(site)) {
-    return site;
-  }
+	if (site && /^https:\/\/lichess\.org\/.{8}/.test(site)) {
+		return site;
+	}
 
-  const queryString = new URLSearchParams({ pgn }).toString();
+	const queryString = new URLSearchParams({ pgn }).toString();
 
-  try {
-    const res = await fetch("https://lichess.org/api/import", {
-      headers: {
-        "content-type": "application/x-www-form-urlencoded",
-      },
-      body: queryString,
-      method: "POST",
-    });
+	try {
+		const res = await fetch("https://lichess.org/api/import", {
+			headers: {
+				"content-type": "application/x-www-form-urlencoded",
+				accept: "application/json",
+			},
+			body: queryString,
+			method: "POST",
+		});
 
-    if (res.status === 200) {
-      const data = await res.json();
-      return data.url;
-    } else {
-      throw new Error("Cannot import to lichess");
-    }
-  } catch (e) {
-    throw new Error("Cannot import to lichess");
-  }
+		if (res.status === 200) {
+			const data = await res.json();
+			return data.url;
+		} else {
+			console.log(res.status);
+			throw new Error("Cannot import to lichess");
+		}
+	} catch (e) {
+		console.log(e);
+		throw new Error("Cannot import to lichess");
+	}
 };
 
 export default importToLichess;
